6ed28243681bcc05391ff2756198e917be2238d4
Unknown <paosalcedo@gmail.com>
2017-03-31 21:35:58 -0400

    Tried different versions of movement. Still not ideal

---
+++ VR Final/Assets/Scripts/MovementScriptV2.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScriptV2 : MonoBehaviour {
+	
+	Vector3 newDir;
+	public float forwardForce = 50f;
+	public float redirectForce = 100f;
+	Rigidbody rb;
+	public float raycastRange = 5f;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveForward();
+	}
+
+	void MoveForward ()
+	{
+//		transform.Translate(transform.forward * forwardForce * Time.deltaTime);
+//		transform.Rotate(Vector3.up * 25f * Time.deltaTime);
+		rb.AddForce (transform.forward * forwardForce * Time.deltaTime, ForceMode.Impulse);
+		Ray ray = new Ray (transform.position, transform.forward);
+		Debug.DrawRay (ray.origin, ray.direction * raycastRange, Color.red);
+		RaycastHit rayHit = new RaycastHit ();
+		
+		if (Physics.Raycast (ray, out rayHit, raycastRange)) {
+			transform.forward = rayHit.normal;
+//			transform.forward = Vector3.Lerp(transform.forward, rayHit.normal, 0.9f);
+ //			transform.forward = newDir;
+			if (rayHit.transform.tag == "Wall") {
+				rb.AddForce ((rayHit.normal + Random.insideUnitSphere) * redirectForce * Time.deltaTime, ForceMode.Impulse);	
+			}  
+		}
+
+
+
+
+//		MOVEMENT USING OTHER RAYS
+//		Ray rayRight = new Ray (transform.position, transform.right);
+//		Ray rayLeft = new Ray (transform.position, Vector3.left);
+//		Ray rayUp = new Ray (transform.position, transform.up); 
+//		Ray rayDown = new Ray (transform.position, Vector3.down);
+//		Ray rayBack = new Ray (transform.position, -transform.forward);
+//			
+//		RaycastHit rayHitUp = new RaycastHit ();
+//		RaycastHit rayHitDown = new RaycastHit ();
+//		RaycastHit rayHitLeft = new RaycastHit ();
+//		RaycastHit rayHitRight = new RaycastHit ();
+//		RaycastHit rayHitBack = new RaycastHit ();
+//
+//		Debug.DrawRay (rayRight.origin, rayRight.direction * raycastRange, Color.red);
+//		Debug.DrawRay (rayLeft.origin, rayLeft.direction * raycastRange, Color.red);	
+//		Debug.DrawRay (rayUp.origin, rayUp.direction * raycastRange, Color.red);
+//		Debug.DrawRay (rayDown.origin, rayDown.direction * raycastRange, Color.red);		
+//		Debug.DrawRay (rayBack.origin, rayBack.direction * raycastRange, Color.red);		
+//		
+//
+//		if (Physics.Raycast (rayUp, out rayHitUp, raycastRange)) {
+//			if (rayHitUp.transform.tag == "Wall") {
+//				rb.AddForce(rayHitUp.normal + Random.insideUnitSphere * redirectForce * Time.deltaTime, ForceMode.Impulse);	
+//			} 
+//		}  
+//		
+//		if (Physics.Raycast (rayDown, out rayHitDown, raycastRange)) {
+//			if (rayHitDown.transform.tag == "Wall") {
+//				rb.AddForce(rayHitDown.normal + Random.insideUnitSphere * redirectForce * Time.deltaTime, ForceMode.Impulse);	
+//			} 
+//		}
+//
+//		if (Physics.Raycast (rayLeft, out rayHitLeft, raycastRange)) {
+//			if (rayHitLeft.transform.tag == "Wall") {
+//				rb.AddForce(rayHitLeft.normal + Random.insideUnitSphere * redirectForce * Time.deltaTime, ForceMode.Impulse);
+//			} 
+//		}
+//
+//		if (Physics.Raycast (rayRight, out rayHitRight, raycastRange)) {
+//			if (rayHitRight.transform.tag == "Wall") {
+//				rb.AddForce(rayHitRight.normal + Random.insideUnitSphere * redirectForce * Time.deltaTime, ForceMode.Impulse);
+//			} 
+//		}
+//		
+//		if (Physics.Raycast (rayBack, out rayHitBack, raycastRange)) {
+//			if (rayHitBack.transform.tag == "Wall") {
+//				rb.AddForce(rayHitBack.normal + Random.insideUnitSphere * redirectForce * Time.deltaTime, ForceMode.Impulse);
+//			} 
+//		}
+
+	}
+}
