a880ec91c72ed35e35e3a64d4833032f790150f7
Unknown <paosalcedo@gmail.com>
2017-05-09 00:03:40 -0400

    Pushed the correct script (DestroyCrumb)

--- VR Final/Assets/Scripts/DestroyCrumb.cs
+++ VR Final/Assets/Scripts/DestroyCrumb.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyCrumb : MonoBehaviour {
-
+	GameObject bug;
+	public float distToBug;
 	// Use this for initialization
 	void Start () {
-		
+		bug = GameObject.Find("BugPrefab");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		if (Vector3.Distance (transform.position, bug.transform.position) <= distToBug) {
+			Destroy(gameObject);
+	 		MoveToCrumb.crumbs.Remove (gameObject);
+		}
 	}
 
 	void OnCollisionEnter(Collision coll){
