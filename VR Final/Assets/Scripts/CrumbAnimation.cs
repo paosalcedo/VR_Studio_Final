bb492af1d98d56ab3fe2f745c6bfa200baa185c6
Unknown <paosalcedo@gmail.com>
2017-05-14 17:54:16 -0400

    Added CrumbAnimation script with bools for animation

---
+++ VR Final/Assets/Scripts/CrumbAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrumbAnimation : MonoBehaviour {
+
+	public Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onCollisionEnter (Collision coll)
+	{
+		if (coll.gameObject.tag == "Ground") {
+			anim.SetBool ("Thrown", false);			
+			anim.SetBool ("hitGround", true);		
+		}		 
+	}
+
+	public void RollUp(){
+		anim.SetBool("Thrown" , true);
+	}
+}
